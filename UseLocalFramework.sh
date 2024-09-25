a42c86beb9f65390d161781329a43f8930a1cd1b
CattySteve <cattysteve89265@163.com>
2024-09-25 09:47:00 +0800

    chore: update csproj to adapt .NET9 and local framework
    All csproj modified to use net9.0. Add local osu-framework.

--- UseLocalFramework.sh
+++ UseLocalFramework.sh
@@ -14,32 +14,32 @@ dotnet remove $GAME_CSPROJ reference ppy.osu.Framework
 dotnet remove $ANDROID_PROPS reference ppy.osu.Framework.Android
 dotnet remove $IOS_PROPS reference ppy.osu.Framework.iOS
 
-dotnet sln $SLN add ../osu-framework/osu.Framework/osu.Framework.csproj \
-    ../osu-framework/osu.Framework.NativeLibs/osu.Framework.NativeLibs.csproj \
-    ../osu-framework/osu.Framework.Android/osu.Framework.Android.csproj \
-    ../osu-framework/osu.Framework.iOS/osu.Framework.iOS.csproj
+dotnet sln $SLN add ../osu-framework-loongarch/osu.Framework/osu.Framework.csproj \
+    ../osu-framework-loongarch/osu.Framework.NativeLibs/osu.Framework.NativeLibs.csproj \
+    ../osu-framework-loongarch/osu.Framework.Android/osu.Framework.Android.csproj \
+    ../osu-framework-loongarch/osu.Framework.iOS/osu.Framework.iOS.csproj
 
-dotnet add $GAME_CSPROJ reference ../osu-framework/osu.Framework/osu.Framework.csproj
-dotnet add $ANDROID_PROPS reference ../osu-framework/osu.Framework.Android/osu.Framework.Android.csproj
-dotnet add $IOS_PROPS reference ../osu-framework/osu.Framework.iOS/osu.Framework.iOS.csproj
+dotnet add $GAME_CSPROJ reference ../osu-framework-loongarch/osu.Framework/osu.Framework.csproj
+dotnet add $ANDROID_PROPS reference ../osu-framework-loongarch/osu.Framework.Android/osu.Framework.Android.csproj
+dotnet add $IOS_PROPS reference ../osu-framework-loongarch/osu.Framework.iOS/osu.Framework.iOS.csproj
 
 # workaround for dotnet add not inserting $(MSBuildThisFileDirectory) on props files
-sed -i.bak 's:"..\\osu-framework:"$(MSBuildThisFileDirectory)..\\osu-framework:g' ./osu.Android.props && rm osu.Android.props.bak
-sed -i.bak 's:"..\\osu-framework:"$(MSBuildThisFileDirectory)..\\osu-framework:g' ./osu.iOS.props && rm osu.iOS.props.bak
+sed -i.bak 's:"..\\osu-framework-loongarch:"$(MSBuildThisFileDirectory)..\\osu-framework-loongarch:g' ./osu.Android.props && rm osu.Android.props.bak
+sed -i.bak 's:"..\\osu-framework-loongarch:"$(MSBuildThisFileDirectory)..\\osu-framework-loongarch:g' ./osu.iOS.props && rm osu.iOS.props.bak
 
 # needed because iOS framework nupkg includes a set of properties to work around certain issues during building,
 # and those get ignored when referencing framework via project, threfore we have to manually include it via props reference.
 sed -i.bak '/<\/Project>/i\
-  <Import Project=\"$(MSBuildThisFileDirectory)../osu-framework/osu.Framework.iOS.props\"/>\
+  <Import Project=\"$(MSBuildThisFileDirectory)../osu-framework-loongarch/osu.Framework.iOS.props\"/>\
 ' ./osu.iOS.props && rm osu.iOS.props.bak
 
 tmp=$(mktemp)
 
-jq '.solution.projects += ["../osu-framework/osu.Framework/osu.Framework.csproj", "../osu-framework/osu.Framework.NativeLibs/osu.Framework.NativeLibs.csproj"]' osu.Desktop.slnf > $tmp
+jq '.solution.projects += ["../osu-framework-loongarch/osu.Framework/osu.Framework.csproj", "../osu-framework-loongarch/osu.Framework.NativeLibs/osu.Framework.NativeLibs.csproj"]' osu.Desktop.slnf > $tmp
 mv -f $tmp osu.Desktop.slnf
 
-jq '.solution.projects += ["../osu-framework/osu.Framework/osu.Framework.csproj", "../osu-framework/osu.Framework.NativeLibs/osu.Framework.NativeLibs.csproj", "../osu-framework/osu.Framework.Android/osu.Framework.Android.csproj"]' osu.Android.slnf > $tmp
+jq '.solution.projects += ["../osu-framework-loongarch/osu.Framework/osu.Framework.csproj", "../osu-framework-loongarch/osu.Framework.NativeLibs/osu.Framework.NativeLibs.csproj", "../osu-framework-loongarch/osu.Framework.Android/osu.Framework.Android.csproj"]' osu.Android.slnf > $tmp
 mv -f $tmp osu.Android.slnf
 
-jq '.solution.projects += ["../osu-framework/osu.Framework/osu.Framework.csproj", "../osu-framework/osu.Framework.NativeLibs/osu.Framework.NativeLibs.csproj", "../osu-framework/osu.Framework.iOS/osu.Framework.iOS.csproj"]' osu.iOS.slnf > $tmp
+jq '.solution.projects += ["../osu-framework-loongarch/osu.Framework/osu.Framework.csproj", "../osu-framework-loongarch/osu.Framework.NativeLibs/osu.Framework.NativeLibs.csproj", "../osu-framework-loongarch/osu.Framework.iOS/osu.Framework.iOS.csproj"]' osu.iOS.slnf > $tmp
 mv -f $tmp osu.iOS.slnf
